0bc8644b927de2528677ac66c987f014855d8499
Todd Hancock <thancock20@hotmail.com>
2017-08-08 22:09:50 -0500

    Setup webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  devServer: {
+    watchContentBase: true
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+};
